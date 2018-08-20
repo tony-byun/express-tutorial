95ea28cce7739bf3590757b5b239906ef65224bc
tony-byun <badblue37@gmail.com>
2018-08-20 14:17:38 +0900

    create user api added

--- router/main.js
+++ router/main.js
@@ -1,9 +1,19 @@
 module.exports = function(app, User)
 {
-    app.get('/',function(req, res){
-        res.render('index', {
-            title: "MY HOMEPAGE",
-            length: 5
-        })
+    app.post('/api/users', function(req, res){
+        var user = new User();
+        user.id = req.body.id;
+        user.name = req.body.name;
+        user.password = req.body.password;
+        user.regdate = new Date(req.body.regdate);
+    
+        user.save(function(err){
+            if(err){
+                console.error(err);
+                res.json({result: 0});
+                return;
+            }
+            res.json({result: 1});
+        });
     });
 }
