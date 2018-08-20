ae1ee8d29d6b8f1def30c2fb85e773eccd06c710
tony-byun <badblue37@gmail.com>
2018-08-20 17:32:00 +0900

    get all/one api added

--- router/main.js
+++ router/main.js
@@ -1,5 +1,24 @@
 module.exports = function(app, User)
 {
+    app.get('/api/users', function(req, res){
+        User.find(function(err, users){
+            if(err){
+                return res.status(500).send({error: err});
+            }
+            res.json(users);
+        })
+    });
+    app.get('/api/users/:user_id', function(req, res){
+        User.findOne({id: req.params.user_id}, function(err, user){
+            if(err){
+                return res.status(500).send({error: err});
+            }
+            if(!user){
+                return res.status(404).json({error: "not found"});
+            }
+            res.json(user);
+        });
+    });
     app.post('/api/users', function(req, res){
         var user = new User();
         user.id = req.body.id;
@@ -9,11 +28,7 @@ module.exports = function(app, User)
 
         // check req validity
         if(!req.body["id"] || !req.body["name"] || !req.body["password"]){
-            res.json({
-                result: 0,
-                error: "invalid request"
-            });
-            return;
+            return res.status(400).json({error: "invalid request"});
         }
 
         user.save(function(err){
@@ -24,5 +39,5 @@ module.exports = function(app, User)
             }
             res.json({result: 1});
         });
-});
+    });
 }
